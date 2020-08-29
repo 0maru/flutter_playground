7d803341b77ac62502ad5082bd24e747ecf2eb3a
maru <0maru.dev@gmail.com>
2020-08-29 23:47:37 +0900

    add router

---
+++ lib/router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter_playground/pages/animation_button.dart';
+import 'package:flutter_playground/pages/index.dart';
+
+class Router {
+  static const initialRoute = IndexPage.path;
+
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case IndexPage.path:
+        return CupertinoPageRoute<void>(
+          builder: (context) => IndexPage(),
+        );
+        break;
+      case AnimationButtonPage.page:
+        return CupertinoPageRoute<void>(
+          builder: (context) => AnimationButtonPage(),
+        );
+        break;
+      default:
+    }
+    assert(false);
+    return null;
+  }
+}
