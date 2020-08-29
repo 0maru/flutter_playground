616be34c7e4796174f98871047e3d0936f4b5eb5
maru <0maru.dev@gmail.com>
2020-08-29 23:46:53 +0900

    add index page

---
+++ lib/pages/index.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IndexPage extends StatelessWidget {
+  static const path = '/index';
+  const IndexPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
