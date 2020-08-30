68f88f06a4d43ae43de4337bc06ade5ce9bec113
maru <0maru.dev@gmail.com>
2020-08-30 23:51:46 +0900

    delete

--- lib/pages/animation_button.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AnimationButtonPage extends StatelessWidget {
-  static const page = '/animation_button_page';
-
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
